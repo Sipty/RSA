f8e2b0eafebf414945066aa738f3ac1e0dc3fd1f
Sipty <choko43@gmail.com>
2013-10-18 15:41:32 +0100

    encrMsg is now an array list

--- src/yotov/c/e/rsa/Crypto.java
+++ src/yotov/c/e/rsa/Crypto.java
@@ -1,15 +1,19 @@
 package yotov.c.e.rsa;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Crypto {
 	
 	// declarations
-	public static int[] encrMsg;
+	//public static int[] encrMsg;
+    private static ArrayList<Integer> encrMsg = new ArrayList<Integer>();
     
 	public static void encrypt(String msg, int e, int n) {
 		// encrypted message array
-		encrMsg = new int[msg.length()];
 		BigInteger exp, 
 			nBig = BigInteger.valueOf(n);
 		// the encrypted msg
@@ -20,24 +24,36 @@ public class Crypto {
 			int c = (int)msg.charAt(i);
 			// encrypting
 			exp = pow(c,e);
-			encrMsg[i] = exp.mod(nBig).intValue();
+			encrMsg.add(exp.mod(nBig).intValue());
 			
-			encr += encrMsg[i]+" ";	// encrypt the message
-			System.out.println("From: "+c+" to "+encrMsg[i]);	// output encryption to console
+			encr += encrMsg.get(i)+" ";	// encrypt the message
+			System.out.println("From: "+c+" to "+encrMsg.get(i));	// output encryption to console
 		}	
 		// write the encrypted message to a file
 		Write.writing(encr, "Encrypted message.txt");
 	}
 	
 	public static void decrypt(int d, int n) {
+		// read encrypted file
+		/*try {
+			Scanner sc = new Scanner(new File("Encrypted message.txt"));
+
+			for(int i=0; sc.hasNextInt(); i++) {
+				encrMsg.add(sc.nextInt());
+			}
+		} catch (IOException e) {
+			System.err.println("Problem writing to file.");
+		}
+		*/
+		
 		// convert to bigint
 		BigInteger exp, result, 
 			nBig = BigInteger.valueOf(n);
 		// the decrypted msg
 		String decr="The original message was: \n";
 		
-		for(int i=0; i<encrMsg.length; i++) {
-			exp = pow(encrMsg[i], d); 
+		for(int i=0; i<encrMsg.size(); i++) {
+			exp = pow(encrMsg.get(i), d); 
 			result = exp.mod(nBig);
 
 			decr += (char)result.intValue();	// decrypt the message
